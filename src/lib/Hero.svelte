910c0cc35c9b613c2e356262086cfea5f97099e5
Andre de Jesus <dejesusandre0226@gmail.com>
2023-02-11 16:53:25 +0800

    progress

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,1 +1,7 @@
-<h1 class="section grid place-items-center">100 vh hero</h1>
+<div class="section grid place-items-center">
+  <h1
+    class="text-5xl text-gray-900 md:text-5xl lg:text-9xl dark:text-white text-center"
+  >
+    hello. i am andre de jesus.
+  </h1>
+</div>
--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -3,17 +3,17 @@
 </script>
 
 <nav
-  class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900"
+  class="px-2 bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700"
 >
   <div class="container flex flex-wrap items-center justify-between mx-auto">
-    <a href="https://flowbite.com/" class="flex items-center">
-      <img src={logo} alt="logo" class="w-12 h-auto" />
+    <a href="#" class="flex items-center">
+      <img src={logo} class="h-6 mr-3 sm:h-10" alt="andeus logo" />
     </a>
     <button
-      data-collapse-toggle="navbar-default"
+      data-collapse-toggle="navbar-dropdown"
       type="button"
       class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-      aria-controls="navbar-default"
+      aria-controls="navbar-dropdown"
       aria-expanded="false"
     >
       <span class="sr-only">Open main menu</span>
@@ -30,36 +30,36 @@
         /></svg
       >
     </button>
-    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+    <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
       <ul
         class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
       >
         <li>
           <a
             href="#"
-            class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-            aria-current="page">about</a
+            class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            >{"<about />"}</a
           >
         </li>
         <li>
           <a
             href="#"
             class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-            >contact</a
+            >{"<contact />"}</a
           >
         </li>
         <li>
           <a
             href="#"
             class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-            >projects</a
+            >{"<projects />"}</a
           >
         </li>
         <li>
           <a
             href="#"
             class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-            >resume</a
+            >{"<resume />"}</a
           >
         </li>
       </ul>
